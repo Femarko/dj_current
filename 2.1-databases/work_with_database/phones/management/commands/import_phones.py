ed76e38dddc5dfa5f8743989a9c4b44e1d09d442
Femarko <fenstermark7@gmail.com>
2023-11-02 00:06:55 +0300

    slug amended

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,5 +1,6 @@
 import csv
 
+import slugify
 from django.core.management.base import BaseCommand
 from phones.models import Phone
 
@@ -18,6 +19,7 @@ class Command(BaseCommand):
                 price=int(phone['price']),
                 image=phone['image'],
                 release_date=phone['release_date'],
-                lte_exists=phone['lte_exists']
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name'])
             )
             entry.save()
