33909d464b5fc78527d184ea132a4ed6f363b02d
Femarko <fenstermark7@gmail.com>
2023-11-12 13:09:03 +0300

    view.py without commented code

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -3,33 +3,6 @@ from django.shortcuts import render
 
 from .models import Student, Teacher
 
-
-# def students_list(request):
-#     SORT_MAP = {
-#         'name': 'name',
-#         'group': 'group',
-#         'teacher': 'teacher'
-#     }
-#
-#     template = 'school/students_list.html'
-#     object_list = Student.objects.order_by('group')
-#     ordering = request.GET.get('group')
-#     if ordering:
-#         object_list = object_list.order_by(SORT_MAP[ordering])
-#     context = {
-#         'object_list': object_list,
-#     }
-#     print(object_list)
-#     for student in object_list:
-#         teachers = student.teachers.all()
-#         print(teachers)
-#
-#     # используйте этот параметр для упорядочивания результатов
-#     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-#     # ordering = 'group'
-#
-#     return render(request, template, context)
-
 def students_list(request):
     template = 'school/students_list.html'
     object_list = Student.objects.all()
