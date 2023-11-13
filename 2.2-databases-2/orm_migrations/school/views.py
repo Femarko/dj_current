ba3c81da7919ea09828103ebf082ab75830bad21
Femarko <fenstermark7@gmail.com>
2023-11-13 22:18:11 +0300

    Students and Teachers: ordering amended in views.py

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,6 +5,7 @@ from .models import Student, Teacher
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all().order_by('group')
+    ordering = 'group'
+    object_list = Student.objects.all().order_by(ordering)
     context = {'object_list': object_list}
     return render(request, template, context)
