d92eb5a567756b59855c3e0132b02fdf42926a17
Femarko <fenstermark7@gmail.com>
2023-11-13 19:14:35 +0300

    Students and Teachers - Final

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,13 +6,5 @@ from .models import Student, Teacher
 def students_list(request):
     template = 'school/students_list.html'
     object_list = Student.objects.all()
-    context = {
-        'object_list': object_list,
-    }
-    print(f'object_list: {object_list}')
-    profs = Teacher.objects.all()
-    print(f'profs: {profs}')
-    for student in object_list:
-        print(f'student: {student}')
-        print(f'student.teachers.all(): {student.teachers.all()}')
+    context = {'object_list': object_list}
     return render(request, template, context)
