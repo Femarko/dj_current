67875314c2ce0e6acdcd7eae28c881b73d664f67
Femarko <fenstermark7@gmail.com>
2023-11-18 01:58:51 +0100

    "m2m-relations": movement towards admin...

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,7 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
