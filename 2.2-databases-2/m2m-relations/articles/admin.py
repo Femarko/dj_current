9f927d87dcc47448d896d0a0f7d26d55d015bbf3
Femarko <fenstermark7@gmail.com>
2023-11-21 23:55:34 +0100

    Inline basically created

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models import Article, Tag, Scope
 
 
+class ScopeInline(admin.TabularInline):
+    model = Scope
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'text', 'published_at', 'image']
+    inlines = [ScopeInline]
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
