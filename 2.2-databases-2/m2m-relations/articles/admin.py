d96edf8bb340a9aa7408388b5e411075929e5f68
Femarko <fenstermark7@gmail.com>
2023-11-23 13:37:49 +0100

    Only one main tag - OK, A main tag exists - problem

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -12,13 +12,14 @@ class ScopeInlineFormset(BaseInlineFormSet):
         for form in self.forms:
             print(f'form.cleaned_data: {form.cleaned_data}')
             print(f'form.cleaned_data.keys(): {form.cleaned_data.keys()}')
-            if 'is_main' not in form.cleaned_data.keys():
+            if 'is_main' not in form.cleaned_data.keys() and list(form.cleaned_data) != []:
                 raise ValidationError('Укажите основной раздел')
             else:
-                if form.cleaned_data['is_main']:
+                if list(form.cleaned_data) != [] and form.cleaned_data['is_main']:
                     counter +=1
+                    print(counter)
         if counter > 1:
-            raise ValidationError('Укажите основной раздел')
+            raise ValidationError('Основным может быть только один раздел')
         else:
             return super().clean()
 
