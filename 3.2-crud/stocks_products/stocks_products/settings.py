d9f6e1cc5b7a7fdfbffeb3893621499b0c350fda
Femarko <fenstermark7@gmail.com>
2023-12-10 22:03:53 +0100

    django_filters included in Settings.py/INSTALLED_APPS

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
-
     'logistic',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
